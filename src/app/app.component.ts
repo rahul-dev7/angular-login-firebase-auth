9374479f574ecd616da3662b03b90336b6b7ef20
Rahul Prajapati <rahul.info567@gmail.com>
2023-05-07 16:19:40 +0530

    setup app is module.

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-root',
+    template: ``
+})
+
+export class AppComponent implements OnInit {
+    constructor() { }
+
+    ngOnInit() { }
+}
