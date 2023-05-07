331c5e3bdffa90eb6692a0e0f25309e769de4305
rahulpra567 <rahul.info567@gmail.com>
2023-05-07 17:14:22 +0530

    setup angular 15

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-    selector: 'app-root',
-    template: ``
+  selector: 'app-root',
+  templateUrl: './app.component.html',
 })
-
 export class AppComponent implements OnInit {
-    constructor() { }
+  constructor() {}
 
-    ngOnInit() { }
+  ngOnInit() {}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
-
-import AppComponent from './app.component.ts';
-
+import { BrowserModule } from '@angular/platform-browser';
+import { AppComponent } from './app.component';
 
 @NgModule({
-    imports: [],
-    exports: [],
-    declarations: [AppComponent],
-    providers: [],
+  imports: [BrowserModule],
+  exports: [],
+  declarations: [AppComponent],
+  providers: [],
+  bootstrap:    [ AppComponent ]
 })
-export class AppModule { }
+export class AppModule {}
