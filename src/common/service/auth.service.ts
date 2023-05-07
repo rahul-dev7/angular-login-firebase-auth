72ced9d2d3f58a3a87586fb6cf22063e7c2fd5d7
Rahul Prajapati <rahul.info567@gmail.com>
2023-05-07 16:14:14 +0530

    auth service is created.

--- src/common/service/auth.service.ts
+++ src/common/service/auth.service.ts
@@ -1,11 +1,44 @@
 import { Injectable } from '@angular/core';
+import { AngularFireAuth } from "@angular/fire/auth";
 
-
-@Injectable()
+@Injectable({
+    providedIn: 'root'
+})
 export class AuthenticationService  {
     constructor(private angularFireAuth: AngularFireAuth) { 
-
-        this.userData = angularFireAuth.authState
+        this.userData = angularFireAuth.authState;
     }
     
-}
+
+    signUp(email: string, password: string) {
+        this.angularFireAuth.auth()
+        .createUserWithEmailWithPassword(email, password)
+        .then(res => {
+            console.log('You are Successfully');
+        })
+        .catch(err => {
+            console.log('Something is wrong:', error.message);
+        })
+
+    }
+
+
+    siginIn() {
+
+        this.angularFireAuth.auth
+        .signInWithEmailAndPassword(email,password)
+        .then(res => {
+            console.log('You are Sucessfully loggedIn');
+        })
+        .catch(err => {
+           console.log('Something is wrong:', err.message) 
+        })
+    }
+
+
+    
+    SignOut() {
+        this.angularFireAuth.auth
+        .signOut()
+    }
+ }
